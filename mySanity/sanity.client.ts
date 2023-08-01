8356d1b54d9b7c3862b9387172049aab43244be7
marc berry <marc.w.berry1@gmail.com>
2023-08-01 11:34:07 -0500

    Got some additional stuff in place

---
+++ mySanity/sanity.client.ts
@@ -0,0 +1,14 @@
+// sanity/sanity.client.ts
+
+import { createClient, type ClientConfig } from "@sanity/client";
+
+const config: ClientConfig = {
+  projectId: "46g2ws23",
+  dataset: "production",
+  apiVersion: "2023-07-16",
+  useCdn: false,
+};
+
+const client = createClient(config);
+
+export default client;
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+// schemas/index.ts
+
+import profile from "./profile";
+import job from "./job";
+import project from "./project";
+
+export const schemaTypes = [profile, job, project];
